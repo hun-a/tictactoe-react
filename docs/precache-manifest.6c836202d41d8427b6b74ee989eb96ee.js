3844ba7fa8b3263ca3714aa752e602f8492baee2
hun-a <seunghun.chan@gmail.com>
2020-07-15 02:12:06 +0900

    Game creation complete

---
+++ docs/precache-manifest.6c836202d41d8427b6b74ee989eb96ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ed0ab3a9b548d817c991d2f7de2340",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f989f14fd7a78baab890",
+    "url": "./static/js/2.70a8d3ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.70a8d3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8e4c8a9bc63dd74ccd",
+    "url": "./static/js/main.16fd0536.chunk.js"
+  },
+  {
+    "revision": "d30d7397c2a16dc47c99",
+    "url": "./static/js/runtime-main.b7e3bdf4.js"
+  }
+]);
